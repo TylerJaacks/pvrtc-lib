61e1739bfd0730c3f6198f2335e20ceae1da209d
Tyler Jaacks <TylerJaacks@Gmail.com>
2023-05-09 22:29:26 -0500

    Fixed code in Rust.

--- src/pvrtc.rs
+++ src/pvrtc.rs
@@ -6,52 +6,53 @@ enum something {
     DXT_MIN_TEXTURE_HEIGHT = 4
 }
 
-struct Pixel32 {
-    u8 r;
-    u8 g;
-    u8 b;
-    u8 a;
+struct pixel_32 {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8
 }
 
-struct Pixel128S {
-    i32 r;
-    i32 g;
-    i32 b;
-    i32 a;
+struct pixel_128_s {
+    r: i32,
+    g: i32,
+    b: i32,
+    a: i32
 }
 
-struct PVRTCWord {
-    u32 modulationData;
-    u32 colorData;
+struct pvrtc_word {
+    modulationData: u32,
+    colorData: u32
 }
 
-struct PVRTCWordIndices {
-    i8 P[2];
-    i8 Q[2];
-    i8 R[2];
-    i8 S[2];
+struct pvrtc_word_indices {
+    P: [i8; 2],
+    Q: [i8; 2],
+    R: [i8; 2],
+    S: [i8; 2]
 }
 
-fn Pixel32 get_color_a(u32 colorData, u32 uiII) -> Pixel32 {
-    Pixel32 color;
+fn get_color_a(color_data: u32, uiII: u32) -> pixel_32 {
+    let mut color: u32;
 
-    if ((colorData & (uiII ? 0x80000000 : 0x80000)) != 0) {
-        color.red = ((colorData & 0x7c00) >> 10);
-        color.green = ((colorData & 0x3e0) >> 5);
-        color.blue = (colorData & 0x1e) | ((colorData & 0x1e) >> 4);
-        color.alpha = 0xf;
-    } else {
-        color.red = ((colorData & 0xf00) >> 7) | ((colorData & 0xf00) >> 11);
-        color.green = ((colorData & 0xf0) >> 3) | ((colorData & 0xf0) >> 7);
-        color.blue = ((colorData & 0xe) << 1) | ((colorData & 0xf) >> 3);
-        color.alpha = ((colorData & 0x7000) >> 11);
-    }
+    // TODO: Figure out how to do this in Rust
+    // if ((color_data & (uiII ? 0x80000000 : 0x80000)) != 0) {
+    //     color.red = ((color_data & 0x7c00) >> 10);
+    //     color.green = ((color_data & 0x3e0) >> 5);
+    //     color.blue = (color_data & 0x1e) | ((color_data & 0x1e) >> 4);
+    //     color.alpha = 0xf;
+    // } else {
+    //     color.red = ((color_data & 0xf00) >> 7) | ((color_data & 0xf00) >> 11);
+    //     color.green = ((color_data & 0xf0) >> 3) | ((color_data & 0xf0) >> 7);
+    //     color.blue = ((color_data & 0xe) << 1) | ((color_data & 0xf) >> 3);
+    //     color.alpha = ((color_data & 0x7000) >> 11);
+    // }
 
     return color;
 }
 
-fn get_color_b(u32 color_data, u32 uiII) -> Pixel32 {
-    Pixle32 color;
+fn get_color_b(color_data: u32, uiII: u32) -> pixel_32 {
+    let mut color: pixel_32;
 
     if (color_data & 0x80000000) {
         color.red = ((color_data & 0x7c000000) >> 26);
@@ -66,16 +67,16 @@ fn get_color_b(u32 color_data, u32 uiII) -> Pixel32 {
     }
 }
 
-fn get_color_ab_expanded(u32 color_data, Pixel128S &color_a, Pixel128S &color_b, u32 ui8Bpp) {
-    Pixel32 color_a_32 = get_color_a(color_data, 1);
-    Pixel32 color_b_32 = get_color_b(color_data, 1);
+fn get_color_ab_expanded(color_data: u32, &color_a: pixel_128_s, &color_b: pixel_128_s, bpp: u32) {
+    let mut color_a_32 = get_color_a(color_data, 1);
+    let mut color_b_32 = get_color_b(color_data, 1);
 
-    colorA =  { color_a_32.red, color_a_32.green, color_a_32.blue, color_a_32.alpha };
-    colorB =  { color_b_32.red, color_b_32.green, color_b_32.blue, color_b_32.alpha };
+    let mut colorA =  { color_a_32.red; color_a_32.green; color_a_32.blue; color_a_32.alpha };
+    let mut colorB =  { color_b_32.red; color_b_32.green; color_b_32.blue; color_b_32.alpha };
 
-    u32 wordWidth = 4;
+    let mut wordWidth = 4;
 
-    if (ui8Bpp == 2) {
+    if (bpp == 2) {
         wordWidth = 8;
     }
 
@@ -89,7 +90,7 @@ fn get_color_ab_expanded(u32 color_data, Pixel128S &color_a, Pixel128S &color_b,
     colorB.blue *= wordWidth * 4;
     colorB.alpha *= wordWidth * 4;
 
-    if (ui8Bpp == 2) {
+    if (bpp == 2) {
         colorA.red = ((colorA.red >> 7) + (colorA.red >> 2));
         colorA.green = ((colorA.green >> 7) + (colorA.green >> 2));
         colorA.blue = ((colorA.blue >> 7) + (colorA.blue >> 2));
@@ -112,21 +113,21 @@ fn get_color_ab_expanded(u32 color_data, Pixel128S &color_a, Pixel128S &color_b,
     }
 }
 
-fn interoplate_colors(Pixel32 P, Pixel32 Q, Pixel32 R, Pixel32 S, Pixel128S *pixel, u8 bpp) {
-    u32 wordWidth = 4;
-    u32 wordHeight = 4;
+fn interoplate_colors(P: pixel_32, Q: pixel_32, R: pixel_32, S: pixel_32, pixel: *mut pixel_32, bpp: u32) {
+    let mut wordWidth = 4;
+    let mut wordHeight = 4;
 
     if (bpp == 2) {
         wordWidth = 8;
     }
 
-    Pixel128S hP = { P.red, P.green, P.blue, P.alpha };
-    Pixel128S hQ = { Q.red, Q.green, Q.blue, Q.alpha };
-    Pixel128S hR = { R.red, R.green, R.blue, R.alpha };
-    Pixel128S hS = { S.red, S.green, S.blue, S.alpha };
+    let mut hP = { P.red; P.green; P.blue; P.alpha };
+    let mut hQ = { Q.red; Q.green; Q.blue; Q.alpha };
+    let mut hR = { R.red; R.green; R.blue; R.alpha };
+    let mut hS = { S.red; S.green; S.blue; S.alpha };
 
-    Pixel128S QminusP = { hQ.red - hP.red, hQ.green - hP.green, hQ.blue - hP.blue, hQ.alpha - hP.alpha };
-    Pixel128S SminusR = { sS.red - hR.red, hS.green - hR.green, hS.blue - hR.blue, hS.alpha - hR.alpha };
+    let mut QminusP = { hQ.red - hP.red; hQ.green - hP.green; hQ.blue - hP.blue; hQ.alpha - hP.alpha };
+    let mut SminusR = { sS.red - hR.red; hS.green - hR.green; hS.blue - hR.blue; hS.alpha - hR.alpha };
 
     hP.red *= wordWidth;
     hP.green *= wordWidth;
@@ -139,11 +140,15 @@ fn interoplate_colors(Pixel32 P, Pixel32 Q, Pixel32 R, Pixel32 S, Pixel128S *pix
     hR.alpha *= wordWidth;
 
     if (bpp == 2) {
-        for (u32 x = 0; x < wordWidth; x++) {
-            Pixel128S result = { 4* hP.red, 4 * hP.green, 4 * hP.blue, 4 * hP.alpha };
-            Pixel128S dY = { hR.red - hp.red, hR.green - hP.green, hR.blue - hP.blue, hR.alpha - hP.alpha };
+        let mut x = 0;
 
-            for (u32 y = 0; y < wordHeight; y++) {
+        for x < wordWidth {
+            let mut result = { 4* hP.red; 4 * hP.green; 4 * hP.blue; 4 * hP.alpha };
+            let mut dY = { hR.red - hp.red; hR.green - hP.green; hR.blue - hP.blue; hR.alpha - hP.alpha };
+
+            let mut y = 0;
+
+            for y < wordHeight {
                 pPixel[y * wordWidth + x].red = ((result.red >> 7) + (result.red >> 2));
                 pPixel[y * wordWidth + x].green = ((result.green >> 7) + (result.green >> 2));
                 pPixel[y * wordWidth + x].blue = ((result.blue >> 7) + (result.blue >> 2));
@@ -166,11 +171,15 @@ fn interoplate_colors(Pixel32 P, Pixel32 Q, Pixel32 R, Pixel32 S, Pixel128S *pix
             hR.alpha += SminusR.alpha;
         }
     } else {
-        for (u32 y = 0; y < wordHeight; y++) {
-            Pixel128S result = { 4* hP.red, 4 * hP.green, 4 * hP.blue, 4 * hP.alpha };
-            Pixel128S dY = { hR.red - hP.red, hR.green - hP.green, hR.blue - hP.blue, hR.alpha - hP.alpha };
+        let mut y = 0;
 
-            for (u32 x = 0; x < wordWidth; x++) {
+        for y < wordHeight {
+            let mut result = { 4* hP.red; 4 * hP.green; 4 * hP.blue; 4 * hP.alpha };
+            let mut dY = { hR.red - hP.red; hR.green - hP.green; hR.blue - hP.blue; hR.alpha - hP.alpha };
+
+            let mut x = 0;
+
+            for x < wordWidth {
                 pixel[y * wordWidth + x].red = ((result.red >> 6) + (result.red >> 1));
                 pixel[y * wordWidth + x].green = ((result.green >> 6) + (result.green >> 1));
                 pixel[y * wordWidth + x].blue = ((result.blue >> 6) + (result.blue >> 1));
@@ -190,17 +199,22 @@ fn interoplate_colors(Pixel32 P, Pixel32 Q, Pixel32 R, Pixel32 S, Pixel128S *pix
     }
 }
 
-fn upack_modulations(const PVRTCWord &word, const PVRTCWord &nmWord, i8 offsetX, i8 offsetY, i32 modulationValues[16][8], i32 modulationModes[16][8], u8 bpp, u32 isII) {
-    u32 wordWidth = word.color_data & 0x1;
-    u32 wordHeight = word.modulationData;
+fn upack_modulations(
+        &word: pvrtc_word, 
+        &nmWord: pvrtc_word, 
+        offsetX: i8, 
+        offsetY: i8, 
+        modulationValues: [[i8; 16]; 8], 
+        modulationModes: [[i8; 16]; 8], 
+        bpp: u8, 
+        isII: u32) {
+    let mut wordWidth = word.color_data & 0x1;
+    let mut wordHeight = word.modulationData;
 
-    u32 hardTransitionBit = nwWord.colorData & (1 << 15);
+    let mut hardTransitionBit = nwWord.colorData & (1 << 15);
 
-    // We need to unpack differently depending on whether we are 2bpp or 4bpp.
     if (bpp == 2) {
         if (wordModMode) {
-            // Determine the mode we are in.
-
             if (modulationBits & 0x1) {
                 if (modulationBits & (0x1 << 20)) {
                     wordModMode = 3;
